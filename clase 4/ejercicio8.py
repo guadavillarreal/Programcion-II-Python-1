1b59d324ddc265ec41c6b6b6453fe7c7392979af
Guadavillarreal <guada.villarreal.gnv@gmail.com>
2024-09-06 01:49:41 -0300

    no toma la f n print para motrar con {}

--- clase 4/ejercicio8.py
+++ clase 4/ejercicio8.py
@@ -1,5 +1,8 @@
-
 #Dado los lados de un rectangulo presentar por pantalla su superficie
 
-l1=int(input("Ingrese un lado del rectangulo"))
-l2=int(input("Ingrese otro lado del rectangulo"))
+ld1=int(input("Ingrese un lado del rectangulo"))
+ld2=int(input("Ingrese otro lado del rectangulo"))
+
+sup = ld1*ld2
+
+print("la superficie del rectangulo es: {sup}")
