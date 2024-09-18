b5a524de30a693664c96499529e989e14547b079
Guadavillarreal <guada.villarreal.gnv@gmail.com>
2024-09-17 21:34:42 -0300

    Ejercicio 1 incompleto

--- Clase 7/ejercicio1.py
+++ Clase 7/ejercicio1.py
@@ -1,11 +1,19 @@
 #1. Dado un numero entero positivo N, presentar por pantalla un mensaje que 
 # indique si dicho número es o no un numero primo
 
-n =int (input("Ingrese un número"))
+n =int (input("Ingrese un número "))
+x= 2
+b=True
 
-if n % 2==0 or n % 3==0 or n % 4==0 or n % 5==0 or n % 6==0 or n %7==0 or n % 2==0 or n % 2==0 or (n != 2 and  n != 3 and  n != 5 and  n != 7 and  n != 9):
-    print ("El número ingresado no es primo")
-else: 
-    print ("El número ingresado es primo")
+while x<n:
+    if n % x==0 :
+        b= True
+    else: 
+        b= False
+    x=x+1
 
+if b == True:
+    print("El número ingresado no es primo")
+else:
+    print("El número ingresado es primo")
 print ("Fin")
--- Clase 7/ejercicio2.py
+++ Clase 7/ejercicio2.py
@@ -1,3 +1,12 @@
 # 2. Ingresar N números enteros, presentar por pantalla el valor del mayor numero 
 # ingresado y la posición en que se ingresó (tener en cuenta la posibilidad de que 
 # todos los números ingresados pueden ser igual)
+
+n = int (input("Ingrese la cantidad de núeros enteros a evaluar: "))
+p=0
+may = 0
+cont =0
+
+while n<may:
+    p=p+1
+
