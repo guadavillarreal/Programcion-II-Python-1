48571aa55b7931f8c64f9a5969508618cfe94edd
guadavillarreal <guada.villarreal.gnv@gmail.com>
2024-09-23 20:22:37 -0300

    Resolucion ejercicio 2- funciona ok

--- Clase 7/ejercicio2.py
+++ Clase 7/ejercicio2.py
@@ -1,12 +1,24 @@
-# 2. Ingresar N números enteros, presentar por pantalla el valor del mayor numero 
-# ingresado y la posición en que se ingresó (tener en cuenta la posibilidad de que 
+# 2. Ingresar N números enteros,
+#  presentar por pantalla el valor del mayor numero ingresado y
+#  la posición en que se ingresó (tener en cuenta la posibilidad de que 
 # todos los números ingresados pueden ser igual)
 
 n = int (input("Ingrese la cantidad de núeros enteros a evaluar: "))
 p=0
 may = 0
-cont =0
+men = 1
+c=0
 
-while n<may:
-    p=p+1
+while c<n:
+    num = int (input("Ingrese un número "))
+    if num > may:
+        may = num
+        p = c+1
+    else:
+        if num < men :
+            men = num
+    c=c+1
 
+print("El menor número ingresado es: ", men)
+print("El mayor número ingresado es: ", may)
+print("La posición en la que se lo ingreso es: ", p)
