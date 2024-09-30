55fde245dc42c8477c28626969ed6652fb8d0f0b
guadavillarreal <guada.villarreal.gnv@gmail.comgit config --global user.emailgit config --global user.email guada.villarreal.gnv@gmail.com>
2024-09-29 21:08:17 -0300

    correcta resolucion de ej 1 y 2 en que use for y range

--- Clase 2/ejercicio01.py
+++ Clase 2/ejercicio01.py
@@ -5,4 +5,4 @@ n1= int (input("Ingrese el primer número: "))
 n2= int (input("Ingrese el segundo número: "))
 rest= n1+n2 
 
-print(f "El Reseultado de a suma de los dos números es: {rest}")
+print( "El Reseultado de a suma de los dos números es:", rest)
