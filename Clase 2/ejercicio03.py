bc658e66b47ea630e42b7dcb34ef590af172ad33
guadavillarreal <guada.villarreal.gnv@gmail.comgit config --global user.emailgit config --global user.email guada.villarreal.gnv@gmail.com>
2024-09-29 21:25:35 -0300

    resolucion del ejercicio 3 funciona correctamente

--- Clase 2/ejercicio03.py
+++ Clase 2/ejercicio03.py
@@ -1,12 +1,14 @@
 #dado un número entero positivo determinar si es o no primo
 
 n1= int (input("Ingrese un número "))
-
-while n1<0 :
-    print("El número ingresado no es correcto ")
-    n1= int (input("Ingrese un número "))
-if (n1%2==0 or n1%3==0 or n1%4==0 or n1%5==0 or n1%6==0 or n1 % 7 == 0 or n1%8==0  or n1 % 9 == 0) and (n1!=2 and n1!=3 and n1!=5 and n1 != 7) :
-#( (n1%2==0 or n1%3==0 or n1%4==0 or n1%5==0 or n1%6==0 or n1%7==0 or n1%8==0 or n1%9==0 ) and (n1!=2 or n1!=3 or n1!=5 or n1!=7)):
-    print("El numero ",n1, " ingresado no es primo ")
+p=2
+b=0
+while p<n1:
+    if n1%p ==0:
+        b+=1
+    p+=1
+    
+if b>=1:
+    print("El número ingresado no es primo")
 else:
     print("El número ingresado es primo")
