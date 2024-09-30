95c79f6ccf55b6648fed21846eb28941efe04ba6
guadavillarreal <guada.villarreal.gnv@gmail.comgit config --global user.emailgit config --global user.email guada.villarreal.gnv@gmail.com>
2024-09-29 22:42:42 -0300

    correcion del ejercicio 5 funciona ok

--- Clase 2/ejercicio02.py
+++ Clase 2/ejercicio02.py
@@ -3,7 +3,14 @@
 
 n1 = int (input("Ingrese un número: "))
 n2 = int (input("Ingrese otro número: "))
+n3=0
 suma=0
+
+if n2<n1:
+    n3=n1
+    n1=n2
+    n2=n3
+
 for numero in range( n1+1 , n2):
     suma+=numero
 print("La suma de los números comprendidos entre los números ingresados es: ", suma)
--- Clase 2/ejercicio05.py
+++ Clase 2/ejercicio05.py
@@ -1,14 +1,12 @@
 #Dado N cifras construye con ellos un numero compuesto
-#no concatena como tiene que ser lo hace con comas el profe lo resolvio en 
-# pizarra ver su resolucion porq creeria que le falta un = en el primer while
-n =int (input("Ingrese la cantidad de cifras a ingresar: "))
-c=0
-n99=0
 
-while c<n :
-    n1 = int (input("Ingrese la primera cifra "))
-    n99 = n99, n1
-    c = c +1
+n=int(input("Ingrese la cantidad de cifras del número compuesto a evaluar "))
+numeroCompuesto=""
+p=0
+for i in range(n):
+    p=i+1
+    cifra=int(input("Ingrese una cifra: "))
+    numeroCompuesto+= str(cifra)
 
-print(n99)
-print (n1)
+numeroCompuesto=int(numeroCompuesto)
+print(numeroCompuesto)
