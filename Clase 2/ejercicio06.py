fddb76a2fef4b8b21b3414949eccd9e0addb099f
guadavillarreal <guada.villarreal.gnv@gmail.comgit config --global user.emailgit config --global user.email guada.villarreal.gnv@gmail.com>
2024-09-30 23:45:15 -0300

    ejercicio 6 sin prueba

--- Clase 2/ejercicio06.py
+++ Clase 2/ejercicio06.py
@@ -2,4 +2,21 @@
 # construya con ellos un número compuesto
 #el profe creo que lo resolvio en clases verlo
 
-print("Ingrese números enteros positivos")
+n=0
+while n>=0:
+   n=int(input("Ingrese la cantidad de números para trabajar"))
+cont=0
+while cont<n:
+    num=int(input("Ingrese un número entero positivo"))
+    if cont==0:
+        nc=num
+    else:
+         aux=num
+         cc=0
+         while 0<aux:
+            cc=cc+1
+            aux=aux/10
+         nc=nc*(10**cc+num)
+    c=c+1
+
+print(cc)
