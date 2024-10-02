7ee384c512f6e939a0e7d62cd3ba5ea9f8cc458b
guadavillarreal <guada.villarreal.gnv@gmail.com>
2024-10-02 20:57:20 -0300

    Resolucion y prueva de ejercicio 6 clase 2

--- Clase 2/ejercicio06.py
+++ Clase 2/ejercicio06.py
@@ -3,20 +3,21 @@
 #el profe creo que lo resolvio en clases verlo
 
 n=0
-while n>=0:
-   n=int(input("Ingrese la cantidad de números para trabajar"))
+while n<=0:
+   n=int(input("Ingrese la cantidad de números para trabajar "))
+   
 cont=0
 while cont<n:
-    num=int(input("Ingrese un número entero positivo"))
+    num=int(input("Ingrese un número entero positivo "))
     if cont==0:
         nc=num
     else:
-         aux=num
+         aux=num 
          cc=0
          while 0<aux:
-            cc=cc+1
-            aux=aux/10
-         nc=nc*(10**cc+num)
-    c=c+1
+            cc=cc+1 
+            aux=aux//10
+         nc=nc*(10**cc)+num
+    cont=cont+1
 
-print(cc)
+print("El número compuesto creado es: ",nc)
