77f09a967f09c4abbab8d85a14d6a79578cc2d34
guadavillarreal <guada.villarreal.gnv@gmail.com>
2024-10-02 23:37:49 -0300

    Resolucion ejercicio 1 clase 3 funciona pero hay que repasarlo

--- Clase 3/ejercicio1.py
+++ Clase 3/ejercicio1.py
@@ -1,10 +1,22 @@
 #Dado un número entero positivo no múltiplo de 10 presente
-#  en pantalla el valor dado y su invertido
+# en pantalla el valor dado y su invertido
 
-n1 = int(input("Ingrese un número entero positivo no multiplo de 10:"))
+n1 = int(input("Ingrese un número entero positivo no multiplo de 10: "))
 
-while n1<= 0 and n1 %10 ==0:
+while n1<=0 or n1%10==0:
+    print("ingreso al bucle")
     print ("Ingrese un número valido")
-    n1 = int(input("Ingrese un número entero positivo no multiplo de 10:"))
+    n1 = int(input("Ingrese un número entero positivo no multiplo de 10: "))
+inv = 0
+aux=n1
+c=0
+while n1>0:
+    inv= inv*10+(n1%10)#0+(1)=5#   5*10+(0)=50
+    n1=n1//10          #51/10=5#   5/10=0
+
+
+#n2=n1**(-1)
+print(aux)
+print(inv)
 
 
